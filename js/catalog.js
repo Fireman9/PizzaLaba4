153a6dd01e9b19510b3b79ba769195e6eef08f63
Fireman9 <54212619+Fireman9@users.noreply.github.com>
2020-11-23 18:49:38 +0200

    Added catalog page

--- js/catalog.js
+++ js/catalog.js
@@ -1,7 +1,43 @@
 import {content} from "./router.js";
 
-let mainHtml = "<div>Catalog Page</div>";
+let catalogHtml = "";
 
 export function setCatalogPage() {
-    content.innerHTML = mainHtml;
+    createCatalog(function (catalogHtml) {
+        content.innerHTML = catalogHtml;
+    })
+}
+
+async function createCatalog(callback) {
+    let response = await fetch('https://my-json-server.typicode.com/Fireman9/PizzaLaba4/db');
+    let db;
+    if (response.ok) {
+        db = await response.json();
+    } else {
+        console.log("Error fetch in catalog");
+        return;
+    }
+    for (let i = 0; i < db.categories.length; i++) {
+        catalogHtml += "<div class='category'>Піца: " + db.categories[i].name + "</div>";
+        catalogHtml += "<div class='productsContainer'>";
+        for (let j = 0; j < db.products.length; j++) {
+            if (db.products[j].categoryID === i) {
+                let productHtml = "<div class='product'>" +
+                    "<div class='productImageDiv'>" +
+                    "<img class='productImage' src='" + db.products[j].image + "' alt>" +
+                    "<div class='productWeight'>" + db.products[j].weight + " г</div>" +
+                    "</div> " +
+                    "<div class='productName'>" + db.products[j].name + "</div> " +
+                    "<div class='productDesc'><b>Індгредієнти:</b><br>" + db.products[j].description + "</div> " +
+                    "<div class='priceAndAddToCart'>" +
+                    "<div class='productPrice'>" + db.products[j].price + " грн</div>" +
+                    "<button class='addToCart'>В кошик</button>" +
+                    "</div> " +
+                    "</div>";
+                catalogHtml += productHtml;
+            }
+        }
+        catalogHtml += "</div>";
+    }
+    callback(catalogHtml);
 }
